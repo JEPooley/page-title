b5e020924823f7ff3ae7342f078ce83983812188
Josh Pooley <joshpooley93@gmail.com>
2022-08-01 22:54:45 +0100

    add TODO

--- page_title/__init__.py
+++ page_title/__init__.py
@@ -1,1 +1,12 @@
 __version__ = '0.1.0'
+
+"""
+TODO:
+    - Add check for supported filetypes within root directory
+    - Add check for existing title
+    - Add docstrings
+    - Add README
+    - Add logs
+    - Add CLI interface
+    - Push to pypi
+"""
